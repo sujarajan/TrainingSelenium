2cdde1a11f023a568dfcde58d8b87bafb8cd97f0
sujarajan <suja.rajan20@gmail.com>
2022-04-02 12:33:44 +0530

    2nd commit from bash

---
+++ src/test/java/SampleAutomation/AB.java
@@ -0,0 +1,10 @@
+package SampleAutomation;
+public class AB{
+
+public static void main(String[] args)
+{
+
+System.out.println("File manually added");
+}
+
+}
--- src/test/java/SampleAutomation/Suja.java
+++
@@ -1,9 +0,0 @@
-public class AB{
-
-public static void main(String[] args)
-{
-
-System.out.println("File manually added"
-}
-
-}
